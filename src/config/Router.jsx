e0f902fa9f66e339e878dfb6cc454bb8e70c7572
lolyg <lolygrana22@hotmail.com>
2023-04-18 17:30:28 -0300

    Oye terminada

---
+++ src/config/Router.jsx
@@ -0,0 +1,44 @@
+import "../estilos/App.css"
+import Canciones from "../pages/Canciones";
+import Iniciar from "../pages/Iniciar";
+import Registro from "../pages/Registro";
+import App from "../pages/App";
+//import Contacto from "../pages/Contacto";
+import LayoutRoot from "../layout/LayoutRoot";
+import { createBrowserRouter } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LayoutRoot />,
+        children: [
+            {
+                index: true,
+                element: <App/>
+            },
+            {
+                path: "Canciones",
+                element: <Canciones />
+            },
+            {
+                path: "Iniciar",
+                element: <Iniciar />,
+            },
+            {
+                path: "Registro",
+                element: <Registro />,
+            },
+            // {
+            //     path: "Contacto",
+            //     element: <Contacto />
+            // },
+        ],
+    },
+]);
+
+export default router;
+
+
+
+
